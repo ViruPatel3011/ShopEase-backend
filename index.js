ea8c8599a5180b2e022fee0a5aef1ea4fd426da7
ViruPatel3011 <pviral3011@gmail.com>
2024-09-18 18:17:44 +0530

    install stripe package

--- index.js
+++ index.js
@@ -119,6 +119,38 @@ passport.deserializeUser(function (user, cb) {
     });
 });
 
+// Payments
+
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+const stripe = require("stripe")('sk_test_tR3PYbcVNZZ796tH88S4VQ2u');
+
+const calculateOrderAmount = (items) => {
+    return 1500;
+};
+
+server.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "usd",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+        // [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
+        dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
+    });
+});
+
+
 main().catch(err => console.log(err));
 
 async function main() {
